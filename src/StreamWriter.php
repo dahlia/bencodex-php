26d35aaae31253b598f1ae00f9967be896325b3c
Hong Minhee <hong.minhee@gmail.com>
2021-10-17 01:40:27 +0900

    StreamWriter

---
+++ src/StreamWriter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Bencodex;
+
+/**
+ * A writer that put the given byte string into a stream, which usually refers
+ * to a file.
+ */
+class StreamWriter implements Writer
+{
+    private $handle;
+
+    /**
+     * Creates a new {@see StreamWriter} instance with the given handle.
+     * @param resource $handle The pointer resource which refers a writable
+     *                         stream, usually a file.
+     */
+    public function __construct($handle)
+    {
+        if (!is_resource($handle)) {
+            throw new \TypeError('Required a file system pointer resource.');
+        }
+
+        $this->handle = $handle;
+    }
+
+    /**
+     * Gets the pointer resource of the stream handle, which usually refers
+     * a file, to write bytes into.
+     * @return resource The handle resource which was configured using
+     *                  the constructor.
+     */
+    public function getHandle()
+    {
+        return $this->handle;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function write($bytes)
+    {
+        if (!is_string($bytes)) {
+            throw new \TypeError('Required a byte string.');
+        }
+        if (!empty($bytes)) {
+            fwrite($this->handle, $bytes);
+        }
+    }
+}
---
+++ test/StreamWriterTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bencode\Tests;
+
+use Bencodex\StreamWriter;
+use Bencodex\Test\TestUtils;
+use PHPUnit\Framework\TestCase;
+
+class StreamWriterTest extends TestCase
+{
+    use TestUtils;
+
+    public function testConstruct()
+    {
+        $this->assertThrows('TypeError', function () {
+            new StreamWriter('not a resource');
+        });
+    }
+
+    public function testWrite()
+    {
+        $file = tmpfile();
+        $this->assertNotFalse($file);
+        $w = new StreamWriter($file);
+        $w->write('');
+        fflush($file);
+        $this->assertEquals(0, ftell($file));
+        fseek($file, 0);
+        $this->assertEmpty(fread($file, 1));
+        fseek($file, 0, SEEK_END);
+        $w->write('foo');
+        fflush($file);
+        $this->assertEquals(3, ftell($file));
+        fseek($file, 0);
+        $this->assertEquals('foo', fread($file, 4));
+        fseek($file, 0, SEEK_END);
+        $w->write('bar');
+        fflush($file);
+        $this->assertEquals(6, ftell($file));
+        fseek($file, 0);
+        $this->assertEquals('foobar', fread($file, 7));
+        fseek($file, 0, SEEK_END);
+        $w->write('....');
+        fflush($file);
+        $this->assertEquals(10, ftell($file));
+        fseek($file, 0);
+        $this->assertEquals('foobar....', fread($file, 11));
+        fseek($file, 0, SEEK_END);
+        $this->assertThrows('TypeError', function () use ($w) {
+            $w->write(1234);
+        });
+    }
+
+    public function testGetHandle()
+    {
+        $file = tmpfile();
+        $this->assertNotFalse($file);
+        $w = new StreamWriter($file);
+        $this->assertSame($file, $w->getHandle());
+    }
+}
