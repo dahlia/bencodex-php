0ab7f0949c0b2ccaa7ee41bbcef2f6ea01e47497
Hong Minhee <hong.minhee@gmail.com>
2021-10-17 01:39:33 +0900

    PHP Coding Standards Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        __DIR__ . "/src",
+        __DIR__ . "/test",
+    ]);
+
+$config = (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR1' => true,
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short']
+    ])
+    ->setFinder($finder);
+
+return $config;
