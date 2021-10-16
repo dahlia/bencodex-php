d6e923fe6847918e9fac58dde45f71b02dae5d60
Hong Minhee <hong.minhee@gmail.com>
2021-10-17 01:37:53 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Bencodex reader/writer for PHP
+==============================
+
+This package implements [Bencodex] serialization format which extends
+[Bencoding].
+
+[Bencodex]: https://bencodex.org/
+[Bencoding]: https://www.bittorrent.org/beps/bep_0003.html#bencoding
---
+++ src/Writer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bencodex;
+
+/**
+ * An interface to write a byte string (which can be quite long) in
+ * separated multiple chunks.
+ */
+interface Writer
+{
+    /**
+     * Writes the given chunk of bytes.
+     * @param string $bytes The byte string to write.  This is usually a chunk
+     *                      of a longer byte string.
+     * @return void
+     * @throws \TypeError Thrown when the given bytes is not a string value.
+     */
+    public function write($bytes);
+}
